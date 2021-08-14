05c40994c8d722d5639bd277f03318b857d13dba
Shobhit Tripathi <61062688+ShobhitTripathi@users.noreply.github.com>
2021-08-14 08:46:31 +0530

    Add files via upload

---
+++ Strings/LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+package ArraysAndString;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+
+    public static String longestCommonPrefixCompute (String[] arr) {
+        Arrays.sort(arr);
+
+        String first = arr[0];
+        String last = arr[arr.length - 1];
+
+        int index = 0;
+        while (index < arr[0].length()) {
+            if (first.charAt(index) == last.charAt(index)) {
+                index++;
+            } else {
+                break;
+            }
+        }
+
+        return index == 0 ? "" : first.substring(0, index);
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"flower", "flow","flight"};
+        String ans = longestCommonPrefixCompute(arr);
+        System.out.println(ans);
+    }
+}
